b9346d1336f0f640cf82a601175a1daa5f1caa0c
llooizaq13 <lluizacavalcante@hotmail.com>
2024-10-26 19:04:02 -0300

    Update tiposPrimitivos.js

--- tiposPrimitivos.js
+++ tiposPrimitivos.js
@@ -1,13 +1,3 @@
-ola = 123
-console.log(ola)*/
-
-const num1 = 10;
-const num2 = 20;
-const calc = num1/num2
-
-console.log(soma)
-
-
 console.log(); console.log("Atividade 07: Imposto de renda")
 
 var salarioBruto = 
