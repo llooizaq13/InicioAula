6fd49c2795d86276e42f64dcdd772550b0f4e5cb
llooizaq13 <lluizacavalcante@hotmail.com>
2024-10-26 18:42:29 -0300

    Update tiposPrimitivos.js

--- tiposPrimitivos.js
+++ tiposPrimitivos.js
@@ -6,3 +6,11 @@ const num2 = 20;
 const calc = num1/num2
 
 console.log(soma)
+
+
+console.log(); console.log("Atividade 07: Imposto de renda")
+
+var salarioBruto = 
+var porcentagem = 
+var descontoSimplificado = salarioBruto * porcentagem / 100
+var salarioLiquido = salarioBruto - descontoSimplificado
