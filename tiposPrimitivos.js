1192eefc30d3ddf448ac4c286ae2b6b9b64d7aed
Maria Luiza Cavalcanti <lluizacavalcante@hotmail.com>
2025-04-01 12:50:36 -0300

    Delete tiposPrimitivos.js

--- tiposPrimitivos.js
+++
@@ -1,6 +0,0 @@
-console.log(); console.log("Atividade 07: Imposto de renda")
-
-var salarioBruto = 
-var porcentagem = 
-var descontoSimplificado = salarioBruto * porcentagem / 100
-var salarioLiquido = salarioBruto - descontoSimplificado
